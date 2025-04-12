e7e705638b3e8e0455d4074a6cf2b980435adf69
Oluwafemi21 <olukoyaoluwafemi14@gmail.com>
2025-04-12 02:16:54 +0100

    icons and wip for hero section

---
+++ src/components/Hero.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="pt-16">
+        <p
+            class="font-semibold w-fit rounded-full bg-primary-300 py-2.5 px-6 text-lg/[22px] mt-1 mb-12"
+        >
+            No-Code WordPress Site Builder
+        </p>
+
+        <div class="mb-24">
+            <h1
+                class="text-8xl/[105px] tracking-tighter capitalize font-semibold"
+            >
+                break limits. <br class="hidden lg:block" />
+                build <span class="text-primary">anything.</span>
+                <br class="hidden lg:block" />
+                no code needed.
+            </h1>
+
+            <div class="grid">
+                <div
+                    class="rounded-xl bg-primary-200 text-lg/[25px] text-font-grey p-6 pb-20"
+                >
+                    <p>
+                        Droip is a no-code, drag-and-drop WordPress builder that
+                        simplifies website creation with powerful capabilities.
+                    </p>
+                </div>
+                <div
+                    class="rounded-xl text-primary gap-1.5 bg-primary-200 text-base/[30px] flex items-center justify-center hover:bg-primary-300 mt-1 mb-2"
+                >
+                    <p>Watch Intro</p>
+                    <img :src="videoIcon" alt="video icon" />
+                </div>
+
+                <button
+                    class="btn-primary text-lg/[28px] group flex items-center justify-center gap-1.5"
+                >
+                    <p class="text-lg/[28px]">Get started with Droip</p>
+                    <HiOutlineChevronRight class="group-hover:hidden" />
+                    <HiArrowRight class="group-hover:block hidden" />
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import videoIcon from "@/assets/icons/video.svg";
+import { HiArrowRight, HiOutlineChevronRight } from "vue-icons-plus/hi";
+</script>
+
+<style scoped></style>
---
+++ src/components/Navigation/TopNav.vue
@@ -0,0 +1,184 @@
+<template>
+    <div class="flex items-center justify-between py-2 gap-16">
+        <img src="@/assets/droip.svg" alt="Droip logo" />
+        <div
+            class="custom:flex items-center justify-between flex-1 hidden h-11"
+        >
+            <!-- navigation buttons -->
+            <div class="flex items-center gap-3.5 text-[14px]">
+                <div
+                    class="group p-2.5 flex items-center cursor-pointer first:text-black nth-[2]:text-black text-font-grey"
+                    :class="{ 'gap-1': hasDropdown }"
+                    v-for="{ label, hasDropdown, links } in navigationLinks"
+                    :key="label"
+                >
+                    <a href="#" class="font-medium capitalize">
+                        {{ label }}
+                    </a>
+                    <BsChevronDown
+                        size="12"
+                        class="group-hover:rotate-180 transition-rotate ease-linear duration-300"
+                        v-if="hasDropdown"
+                    />
+
+                    <div class="bg-purple rounded-lg"></div>
+                </div>
+            </div>
+            <!-- auth buttons -->
+            <div class="flex items-center gap-4">
+                <button class="p-2.5 text-font hover:text-black">Login</button>
+                <div>
+                    <button class="btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+        <button @click="toggleDropdown" class="*:text-primary custom:hidden">
+            <HiOutlineMenu v-if="!showDropdown" />
+            <HiX v-else />
+        </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { BsChevronDown } from "vue-icons-plus/bs";
+import { HiOutlineMenu, HiX } from "vue-icons-plus/hi";
+import accessibilityIcon from "@/assets/icons/accessibility.svg";
+import contentIcon from "@/assets/icons/content.svg";
+import editorIcon from "@/assets/icons/editor.svg";
+import figmaIcon from "@/assets/icons/figma.svg";
+import formBuilderIcon from "@/assets/icons/form-builder.svg";
+import gridIcon from "@/assets/icons/grid.svg"; // or grids.svg if you prefer
+import interactionIcon from "@/assets/icons/interaction.svg";
+import mediaIcon from "@/assets/icons/media.svg";
+import popupIcon from "@/assets/icons/popup.svg";
+import seoIcon from "@/assets/icons/seo.svg";
+import typoIcon from "@/assets/icons/typo.svg";
+import blogIcon from "@/assets/icons/blog.svg";
+import documentationIcon from "@/assets/icons/documentation.svg";
+import clockIcon from "@/assets/icons/clock.svg";
+import messageIcon from "@/assets/icons/message.svg";
+import featureIcon from "@/assets/icons/feature.svg";
+import contactIcon from "@/assets/icons/contact.svg";
+import { ref } from "vue";
+
+const showDropdown = ref(false);
+const navigationLinks = [
+    {
+        label: "home",
+        hasDropdown: false,
+    },
+    {
+        label: "product",
+        hasDropdown: true,
+        links: [
+            {
+                title: "Editor",
+                description: "Effective Visual Builder",
+                icon: editorIcon,
+            },
+            {
+                title: "Interaction & Animation",
+                description: "Create interactive experiences",
+                icon: interactionIcon,
+            },
+            {
+                title: "Grids & Layouts",
+                description: "Structure more easily",
+                icon: gridIcon,
+            },
+            {
+                title: "Media Manager",
+                description: "Manage & edit site assets",
+                icon: mediaIcon,
+            },
+            {
+                title: "Typography",
+                description: "Customize your branding",
+                icon: typoIcon,
+            },
+            {
+                title: "SEO",
+                description: "Optimize your SEO workflow",
+                icon: seoIcon,
+            },
+            {
+                title: "Form Builder",
+                description: "Design any web forms",
+                icon: formBuilderIcon,
+            },
+            {
+                title: "Accessibility",
+                description: "Accessible to everyone",
+                icon: accessibilityIcon,
+            },
+            {
+                title: "Pop-up Builder",
+                description: "Build pop-ups visually",
+                icon: popupIcon,
+            },
+            {
+                title: "Figma to Droip",
+                description: "Turn static design into live pages",
+                icon: figmaIcon,
+            },
+            {
+                title: "Content Manager",
+                description: "Centralized dynamic content management",
+                icon: contentIcon,
+            },
+        ],
+    },
+    {
+        label: "resources",
+        hasDropdown: true,
+        links: [
+            {
+                title: "Droip Blogs",
+                description: "Explore what's happening",
+                icon: blogIcon,
+            },
+            {
+                title: "Documentation",
+                description: "Learn from documentation",
+                icon: documentationIcon,
+            },
+            {
+                title: "Release Notes",
+                description: "Check what's new",
+                icon: clockIcon,
+            },
+        ],
+    },
+    {
+        label: "support",
+        hasDropdown: true,
+        links: [
+            {
+                title: "Get support",
+                description: "Fix your issues with our experts",
+                icon: messageIcon,
+            },
+            {
+                title: "Feature Request",
+                description: "Let us know what's missing",
+                icon: featureIcon,
+            },
+            {
+                title: "Contact",
+                description: "Contact for query",
+                icon: contactIcon,
+            },
+        ],
+    },
+    {
+        label: "pricing",
+        hasDropdown: false,
+    },
+];
+
+const toggleDropdown = () => {
+    showDropdown.value = !showDropdown.value;
+};
+</script>
+
+<style scoped></style>
